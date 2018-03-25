f695bb066a2d91d3eab66a89282191c3973a2324
luke0b1000 <luke0b1000>
2018-03-25 19:23:46 -0400

    .toggleAll: If everything's true, make everything false

--- script.js
+++ script.js
@@ -34,4 +34,24 @@ var todoList = {
         todo.completed = !todo.completed;
         this.displayTodos();
     },
+    toggleAll: function() {
+        var totalTodos = this.todos.length;
+        var completedTodos = 0;
+
+        // Get number of completed todos, so we can match it up against totalTodos
+        for (var i = 0; i < totalTodos; i++) {
+            if (this.todos[i].completed === true) {
+                completedTodos++;
+            }
+        }
+
+        // Case 1: If everything's true, make everything false.
+        if (completedTodos === totalTodos) {
+            for (var i = 0; i < totalTodos; i++) {
+                this.todos[i].completed = false;
+            }
+        }
+
+        this.displayTodos();
+    },
 };
