9a14b9c1eeb0f1d843ba5e0515df4fc7007e4efd
luke0b1000 <luke0b1000>
2018-03-25 22:44:48 -0400

    Escaping the console

--- script.js
+++ script.js
@@ -1,38 +1,20 @@
 var todoList = {
     todos : [],
-    displayTodos: function() {
-        if (this.todos.length === 0) {
-            console.log('YOUr todo list is empty!');
-        } else {
-            console.log('My Todos:');
-            for (var i = 0; i < this.todos.length; i++) {
-                if (this.todos[i].completed === true) {
-                    console.log('(x)', this.todos[i].todoText);
-                } else {
-                    console.log('( )', this.todos[i].todoText);
-                }
-            }
-        }
-    },
     addTodo: function(todoText) {
         this.todos.push({                       // pushing a object into todos Array
             todoText: todoText,
             completed: false,                   // set false by default for completed
         });
-        this.displayTodos();
     },
     changeTodo: function(position, todoText) {
         this.todos[position].todoText = todoText;
-        this.displayTodos();
     },
     deleteTodo: function(position) {
         this.todos.splice(position, 1);
-        this.displayTodos();
     },
     toggleCompleted: function(position) {
         var todo = this.todos[position];
         todo.completed = !todo.completed;
-        this.displayTodos();
     },
     toggleAll: function() {
         var totalTodos = this.todos.length;
@@ -56,19 +38,15 @@ var todoList = {
                 this.todos[i].completed = true;
             }
         }
-
-        this.displayTodos();
     },
 };
 
 var handlers = {                // handlers are use for html -> this (handlers object) -> todoList control
-    displayTodos: function() {  // called by handlers.displayTodos()
-        todoList.displayTodos();
-    },
     addTodo: function() {
         var addTodoTextInput = document.getElementById('addTodoTextInput'); // Retrieve the element of addTodoTextInput
         todoList.addTodo(addTodoTextInput.value);                           // addTodoTextInput.value is the value of what is type in
         addTodoTextInput.value = '';                                        // Because this is retrieving the element, we can manipulate the element and clear it out
+        view.displayTodos();
     },
     changeTodo: function() {
         var changeTodoPositionInput = document.getElementById('changeTodoPositionInput');   // <input id="changeTodoPositionInput" type="number">
@@ -76,24 +54,28 @@ var handlers = {                // handlers are use for html -> this (handlers o
         todoList.changeTodo(changeTodoPositionInput.valueAsNumber, changeTodoTextInput.value)
         changeTodoPositionInput.value = '';
         changeTodoTextInput.value = '';
+        view.displayTodos();
     },
     deleteTodo: function(){
         var deleteTodoPositionInput = document.getElementById('deleteTodoPositionInput');
         todoList.deleteTodo(deleteTodoPositionInput.valueAsNumber);                         // since this is a number
         deleteTodoPositionInput.value = '';
+        view.displayTodos();
     },
     toggleCompleted: function() {
         var toggleCompletedPositionInput = document.getElementById('toggleCompletedPositionInput');
         todoList.toggleCompleted(toggleCompletedPositionInput.valueAsNumber);
         toggleCompletedPositionInput.value = '';
+        view.displayTodos();
     },
     toggleAll: function() {     // called by handlers.toggleAll()
         todoList.toggleAll();
+        view.displayTodos();
     },
 };
 
 var view = {                                        
-    displayTodos: function() {                      // view.displayTodos();
+    displayTodos: function() {                      // view.displayTodos();  Added at the end of every handler
         var todoUl = document.querySelector('ul');  // Get me the element <ul> and everything in its child
         todoUl.innerHTML = '';                      // reset the <ul> before putting the list in 
         for (var i = 0; i < todoList.todos.length; i++) {
